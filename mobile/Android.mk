035555b05ce36a566d7b06d5c66ca1532101d3e2
Emile Belanger <emile.belanger@gmail.com>
2024-01-16 17:39:54 +0000

    Fix to use separate ogg vorbis library

--- mobile/Android.mk
+++ mobile/Android.mk
@@ -121,7 +121,7 @@ LOCAL_SRC_FILES :=  $(OBJS:.o=.c) \
 
 LOCAL_LDLIBS := -lEGL -ldl -llog -lz
 LOCAL_STATIC_LIBRARIES := sigc libzip libpng logwritter  mpg123 ifaddrs mad
-LOCAL_SHARED_LIBRARIES := touchcontrols SDL2 SDL2_mixer core_shared GL4ES saffal
+LOCAL_SHARED_LIBRARIES := touchcontrols libvorbis libogg SDL2 SDL2_mixer core_shared GL4ES saffal
 
 include $(BUILD_SHARED_LIBRARY)
 
